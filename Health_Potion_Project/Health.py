9594754d66534550dd698601f741ce23ec932383
Phillip <phillip@royos.fios-router.home>
2018-12-16 16:58:39 -0500

    minor spacing changes in the file

--- Health_Potion_Project/Health.py
+++ Health_Potion_Project/Health.py
@@ -6,16 +6,14 @@ mediumHealthAmount = random.randint(5,9)
 hardHealthAmount = random.randint(1,4)
 charHealth = int(input("give me a number for the health please: "))
 gameDifficulty = str(input("what is the difficulty(easy, medium, hard): "))
-
 finalHealth = int()
+
 def character(charHealth,gameDifficulty):
 
-
-
     if "easy" == gameDifficulty:
-
         print(charHealth, "This is the current health value")
         return(charHealth + easyHealthAmount)
+    
     elif "medium" == gameDifficulty:
         print(charHealth, "This is the current health value")
         return(charHealth + mediumHealthAmount)
