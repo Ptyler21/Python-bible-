10388259885893a0bc42e6ce8fbb728f328b82df
Phillip <phillip@royos.fios-router.home>
2018-12-26 18:04:58 -0500

    the python file for the fourth project

---
+++ Project4/securityrobot.py
@@ -0,0 +1,32 @@
+
+nameList = ["jason","stephan","joseph","phillip"]
+def mainList():
+    userName = input("Hello please give me a name: ")
+    if nameList.count(userName) == 0:
+        print("no entry in database")
+        print(nameList)
+        userNameAdd = input("would you like to add the name?(y/n): ")
+        if "y" == userNameAdd:
+            print("adding name...")
+            nameList.append(userName)
+            print("the name has been added!")
+            print(nameList)
+        elif "n" == userNameAdd:
+            pass
+    elif nameList.count(userName) >= 1:
+        print("the name is in the database!")
+        print(nameList)
+        userInput = input("would you like to delete it?(y/n): ")
+        if "y" == userInput:
+            print("deleting name from list")
+            nameList.remove(userName)
+            print("name deleted....")
+            print(nameList)
+        elif "n" == userInput:
+            pass
+def main():
+    mainList()
+
+
+if __name__=="__main__":
+    main()
