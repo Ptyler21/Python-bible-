2bbcc5473cd15a339a81d2fe89846a9e762be10e
Phillip <phillip@royos.fios-router.home>
2018-12-28 12:50:45 -0500

    Minor update to prject 4

--- Project4/securityrobot.py
+++ Project4/securityrobot.py
@@ -1,29 +1,30 @@
 
 nameList = ["jason","stephan","joseph","phillip"]
 def mainList():
-    userName = input("Hello please give me a name: ")
-    if nameList.count(userName) == 0:
-        print("no entry in database")
-        print(nameList)
-        userNameAdd = input("would you like to add the name?(y/n): ")
-        if "y" == userNameAdd:
-            print("adding name...")
-            nameList.append(userName)
-            print("the name has been added!")
+    while True:
+        userName = input("Hello please give me a name: ")
+        if nameList.count(userName) == 0:
+            print("no entry in database")
             print(nameList)
-        elif "n" == userNameAdd:
-            pass
-    elif nameList.count(userName) >= 1:
-        print("the name is in the database!")
-        print(nameList)
-        userInput = input("would you like to delete it?(y/n): ")
-        if "y" == userInput:
-            print("deleting name from list")
-            nameList.remove(userName)
-            print("name deleted....")
+            userNameAdd = input("would you like to add the name?(y/n): ")
+            if "y" == userNameAdd:
+                print("adding name...")
+                nameList.append(userName)
+                print("the name has been added!")
+                print(nameList)
+            elif "n" == userNameAdd:
+                pass
+        elif nameList.count(userName) >= 1:
+            print("the name is in the database!")
             print(nameList)
-        elif "n" == userInput:
-            pass
+            userInput = input("would you like to delete it?(y/n): ")
+            if "y" == userInput:
+                print("deleting name from list")
+                nameList.remove(userName)
+                print("name deleted....")
+                print(nameList)
+            elif "n" == userInput:
+                pass
 def main():
     mainList()
 
