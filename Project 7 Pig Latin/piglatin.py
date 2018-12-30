fa4a7bc11528f4d848d56842324e944a9c979384
Phillip <phillip@royos.fios-router.home>
2018-12-30 14:43:56 -0500

    Trying to figure out how to two letters at the end of a word

--- Project 7 Pig Latin/piglatin.py
+++ Project 7 Pig Latin/piglatin.py
@@ -6,9 +6,28 @@ pigTestList = sentence.split()
 vowelList = ("a","e","i","o","u","y")
 #check if anywords start with aeiouy
 
-for word in pigTestList:
-    if word.startswith(vowelList[0:]):
+for word in range(len(pigTestList)):
+    if pigTestList[word].startswith(vowelList[0:]):
+        vowelsFound = pigTestList[word] + "yay"
+    elif pigTestList[word] != pigTestList[word].startswith(vowelList[0:]):
+        consonantsFound = pigTestList[word]
+        #print(type(consonantsFound))
+        print(consonantsFound[0])
+        print(consonantsFound[1])
+        '''
+        if len(consonantsFound) > 3:
+            #print(consonantsFound,",greater than three letters")
+            for i in range(len(consonantsFound)):
+                print(consonantsFound[0], ", the first char of the word")
+                print(consonantsFound[1], ", the second char of the word")
+        '''
+        #elif len(consonantsFound) < 3:
+            #print(consonantsFound, ", less than three characters")
 
-        word = word + "yay"
-        print(word)
-        
+
+
+        #get the index of the words that return true
+        #list.index()
+        #print(pigTestList.index(word))
+        #word = word + "yay"
+        #print(word)
