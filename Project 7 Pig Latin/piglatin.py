01413362a94e3ccdcaa8a666490c5791895a8e71
Phillip <phillip@royos.fios-router.home>
2018-12-31 13:21:08 -0500

    Finished the initial algorithm. The next step is to allow user input for the sentence

--- Project 7 Pig Latin/piglatin.py
+++ Project 7 Pig Latin/piglatin.py
@@ -1,20 +1,23 @@
 
-#generate your sentence
 sentence = "will you go with me to the ball to eat curry?"
-#put the word into a list
 pigTestList = sentence.split()
 vowelList = ("a","e","i","o","u","y")
-#check if anywords start with aeiouy
-
 for word in range(len(pigTestList)):
     if pigTestList[word].startswith(vowelList[0:]):
         pigTestList[word] = pigTestList[word] + "yay"
 
     elif pigTestList[word] != pigTestList[word].startswith(vowelList[0:]):
-        if len(pigTestList[word]) > 3:
-            #need to take the first two letters and move them to the end of the word
-            #the strings within this for loop art strings
-            deals = pigTestList[word].lstrip(pigTestList[word][0:2])
-            doneDeal = pigTestList[word][0:2]#taken out the first two letters from each word
-            finalProduct = deals + doneDeal + "yay"
-            pigTestList[word] = finalProduct
+        if len(pigTestList[word]) >= 3:
+            firstTwoLetterStrip = pigTestList[word].lstrip(pigTestList[word][0:2]).strip("?")
+            firstTwoLetterStorage = pigTestList[word][0:2]
+            largerThanThreeConcat = firstTwoLetterStrip + firstTwoLetterStorage.rstrip('?') + "yay"
+            pigTestList[word] = largerThanThreeConcat
+        elif len(pigTestList[word]) < 3:
+            firstLetterStorage = pigTestList[word][0]
+            secondLetterStorage = pigTestList[word][1]
+            twoWordReverse = secondLetterStorage + firstLetterStorage + "yay"
+            pigTestList[word] = twoWordReverse
+
+finalSentence = ' '.join(letter for letter in pigTestList)
+
+print(finalSentence)
