530c0f68c3eca85987dce6594e4104cae72fcffb
Ptyler21 <ptylerjr2@gmail.com>
2019-03-03 00:01:15 -0500

    A completed version of the "Make your own bank!" project. The project is simply meant to show some of the things python classes can do.

--- Project 10 Make a Bank/bank.py
+++ Project 10 Make a Bank/bank.py
@@ -1,38 +1,53 @@
-
-#A class named 'Account'
-#A class for elements that ALL accounts have
 '''
-Abstraction: Account
+Account class
+This class is the abstrated basic elements of an account
 '''
 class Account:
     def __init__(self,accountName,accountBalance,accountMinBalance):
         self.accountName = accountName
         self.accountBalance = accountBalance
         self.accountMinBalance = accountMinBalance
-
-#A function for an action related to an account
 '''
-Action: Deposit
-while performing a deposit: AMOUNT of money, ACCOUNT TYPE
-
+the deposit action within all accounts
 '''
     def deposit(self,amount):
         self.accountBalance += amount
-
 '''
-Action: withdraw
-anyone can withdraw money from either account
--you must have enough money
--overdraft fees
+all accounts can have a withdraw feature
 '''
     def withdraw(self,amount):
-        if amount >= self.accountMinBalance:
+        if self.accountBalance - amount >= self.accountMinBalance:
             self.accountBalance -= amount
-
         else:
-            print("Not enough money available!")
+            print("Sorry not enough funds!")
 '''
-'Action: generate a statement'
+all accounts can print a statement
 '''
     def statement(self):
-        print("Account Balance: {}".format(self.accountBalance))
+        print("Account Balance:$ {}".format(self.accountBalance))
+'''
+This will be a live checking(Current) account
+'''
+class CurrentAccount(Account):
+    def __init__(self,accountName,accountBalance):
+        super().__init__(accountName,accountBalance,accountMinBalance= -1000)
+
+    def __str__(self):
+        return"{}'s current account: Balance ${}".format(self.accountName,self.accountBalance)
+'''
+A live Savings account 
+'''
+class Savings(Account):
+    def __init__(self,accountName,accountBalance):
+        super().__init__(accountName,accountBalance,accountMinBalance= 0)
+
+    def __str__(self):
+        return "{}'s Savings account: Balance ${}".format(self.accountName, self.accountBalance)
+
+
+
+x = CurrentAccount("Phillip",500)
+x.deposit(300)
+x.withdraw(600)
+x.statement()
+print(x)
